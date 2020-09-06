e361a757799a100068cb091978da03e8d9d063fc
Alexandre Figueiredo <ale.fig1701@gmail.com>
2020-09-06 12:41:54 -0300

    Working on image profile source & style from profile items

--- src/Profile.js
+++ src/Profile.js
@@ -9,8 +9,8 @@ const Profile = ({ user }) => (
             <div className="contentBox-info user">
               <h2 className="user-name">{ user.name }</h2>
               <h4 className="user-username">{ user.login }</h4>
-              <img className="user-photo" src="{ user.avatar_url }" alt="Foto de perfil"/>
-              <p>Seguidores <span>{ user.followers }</span> Seguindo <span>{ user.following }</span></p>
+              <img className="user-photo" src={ user.avatar_url } alt="Foto de perfil"/>
+              <p className="user-specs">Seguidores <span>{ user.followers }</span> Seguindo <span>{ user.following }</span></p>
               <h3 className="user-bio">{ user.bio }</h3>
               <p className="user-mail">{ user.email }</p>
             </div>
