c63375efc3dc190c78f9191b6f60b1881c852b2e
Alexandre Figueiredo <ale.fig1701@gmail.com>
2020-09-06 12:31:57 -0300

    Initializing API integration & Profile Component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,59 @@
-import React from 'react';
+import React, { Component } from "react";
+import axios from 'axios';
 import './styles/App.scss';
+import Profile from './Profile';
 
-function App() {
-  return (
-    <div className="App">
-      <header>
-        <div className="container">
-          <div className="row">
-            <div className="column">
-              <div className="searchBox">
-                <h1 className="searchBox-title">RepoSearch</h1>
-                <h3 className="searchBox-subtitle">Insira um username do GitHub e confira seus detalhes</h3>
-                <input id="search" type="text" className="searchBox-form" placeholder="GitHub username" required/>
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      github: {
+        url: "https://api.github.com/users",
+        client_id: "Iv1.3af9a41704e3eae8",
+        client_secret: "58f09be9396ec7c1a288f67465c113a9c8b90984",
+        count: "7",
+        sort: "created: asc"
+      },
+      user: [],
+      repos: []
+    }
+  }
+
+  getUser (e) {
+    const user = e.target.value;
+    const { url, client_id, client_secret, count, sort } = this.state.github;
+      axios
+        .get (
+          `${url}/${user}?client_id=${client_id}&client_secret=${client_secret}`
+        )
+        .then(({ data }) => this.setState({ user:data }));
+  }
+
+  render() {
+    
+    const { user } = this.state;
+
+    return (
+      <div className="App">
+        <header>
+          <div className="container">
+            <div className="row">
+              <div className="column">
+                <div className="searchBox">
+                  <h1 className="searchBox-title">RepoSearch</h1>
+                  <h3 className="searchBox-subtitle">Insira um username do GitHub e confira seus detalhes</h3>
+                  <input onChange={this.getUser.bind(this)} id="search" type="text" className="searchBox-form" placeholder="GitHub username" required/>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-      </header>
-    </div>
-  );
+        </header>
+
+        { user.length !== 0 ? <Profile user={user} /> : null }
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/Profile.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const Profile = ({ user }) => (
+    <main>
+    <div className="container">
+      <div className="contentBox">
+        <div className="row">
+          <div className="column">
+            <div className="contentBox-info user">
+              <h2 className="user-name">{ user.name }</h2>
+              <h4 className="user-username">{ user.login }</h4>
+              <img className="user-photo" src="{ user.avatar_url }" alt="Foto de perfil"/>
+              <p>Seguidores <span>{ user.followers }</span> Seguindo <span>{ user.following }</span></p>
+              <h3 className="user-bio">{ user.bio }</h3>
+              <p className="user-mail">{ user.email }</p>
+            </div>
+          </div>
+          <div className="column _2">
+            
+          </div>
+        </div>
+      </div>
+    </div>
+  </main>
+)
+
+export default Profile;
