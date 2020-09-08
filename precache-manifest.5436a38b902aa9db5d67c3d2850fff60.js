5229d2e14afe929c07be22b386d2b285f3e20104
Alexandre Figueiredo <ale.fig1701@gmail.com>
2020-09-08 02:23:11 -0300

    Updates

---
+++ precache-manifest.5436a38b902aa9db5d67c3d2850fff60.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbf3103c01f0dd411f573a6bf344c0bf",
+    "url": "/RepoSearch/index.html"
+  },
+  {
+    "revision": "dc2989bf78bab18b4d44",
+    "url": "/RepoSearch/static/css/main.48a5c10c.chunk.css"
+  },
+  {
+    "revision": "0ea84dac5c87742da208",
+    "url": "/RepoSearch/static/js/2.0a3383c9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/RepoSearch/static/js/2.0a3383c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc2989bf78bab18b4d44",
+    "url": "/RepoSearch/static/js/main.792104a2.chunk.js"
+  },
+  {
+    "revision": "b0a734f86da2dd5ff15f",
+    "url": "/RepoSearch/static/js/runtime-main.88cbdd9b.js"
+  }
+]);
